981cd44e0ce5d79c3595d10d3e351261dedd8ca6
Wen Li <spacelis@gmail.com>
2016-11-16 00:02:19 +0000

    Add NESS indexing

--- neo4j-plugin/src/main/scala/uk/ac/cdrc/mintsearch/neo4j/NeighborhoodBasedSimilaritySearch.scala
+++ neo4j-plugin/src/main/scala/uk/ac/cdrc/mintsearch/neo4j/NeighborhoodBasedSimilaritySearch.scala
@@ -1,22 +1,22 @@
 package uk.ac.cdrc.mintsearch.neo4j
 
-import org.neo4j.graphdb.index.IndexManager
-import collection.JavaConverters._
+import org.neo4j.graphdb.index.{ Index, IndexManager }
+
 import scala.language.implicitConversions
+import scala.math._
+import collection.JavaConverters._
+import java.util.stream.{ Stream => JStream }
+import java.util.function.{ Function => JFunction }
 
-import java.util.{List => JList}
-import java.util.stream.{Stream => JStream}
-import java.util.function.{Function => JFunction}
-
-import org.neo4j.graphdb.Node
-import org.neo4j.graphdb.RelationshipType
-import org.neo4j.graphdb.traversal.Uniqueness
-import org.neo4j.graphdb.traversal.Evaluators
+import org.neo4j.procedure.Context
+import org.neo4j.graphdb.{ GraphDatabaseService, Node, RelationshipType }
+import org.neo4j.graphdb.traversal.{ Evaluators, TraversalDescription, Uniqueness }
 import org.neo4j.graphdb.index.IndexManager
 import org.neo4j.procedure.Name
 import org.neo4j.procedure.PerformsWrites
 import org.neo4j.procedure.Procedure
 
+case class SearchHit(nodeId: Long)
 /**
  * This class implements the NEighborhood based Similarity Search
  */
@@ -46,39 +46,47 @@ class NeighborhoodBasedSimilaritySearch extends Neo4JProcedure {
   /**
    *
    * @param nodeId the id of the node to index
-   * @param propKeys a list of property keys to index, only the ones the node
-   *                 actually contains will be added
+   * @param relType the relation type used as neighboring
+   * @param propName the property name for the labels, the labels should be stored as a space-separated words
+   * @param depth the label propagation range, i.e., number of hops a label can propagate though neighbor nodes
+   * @param alpha the alpha (propagating damper) and 0 < alpha < 1, i.e., labels are propagated to the neighbor with
+   *              a less weight
    */
   @Procedure("mint.ness_index")
   @PerformsWrites
   def index(
     @Name("nodeId") nodeId: Long,
     @Name("relType") relType: String,
-    @Name("depth") depth: Int
+    @Name("propName") propName: String,
+    @Name("depth") depth: Long,
+    @Name("alpha") alpha: Double
   ): Unit = {
     // TODO Need to add alpha for the propagation of the labels
     val node = db.getNodeById(nodeId)
     val td = db.traversalDescription()
       .relationships(RelationshipType.withName(relType))
       .uniqueness(Uniqueness.NODE_GLOBAL)
-      .evaluator(Evaluators.atDepth(depth))
+      .evaluator(Evaluators.atDepth(depth.toInt))
+    val index = db.index().forNodes(NeighborhoodBasedSimilaritySearch.indexName(propName), NeighborhoodBasedSimilaritySearch.FULL_TEXT.asJava)
+    val labelWeights = propagatedLabels(node, propName, td, alpha)
+    index.remove(node) // Make sure the node will be replaced in the index
+    index.add(node, propName, labelWeights.keys mkString (" "))
+    node.setProperty(NeighborhoodBasedSimilaritySearch.LABEL_PROPERTY_NAME, labelWeights)
+  }
 
-    for ( node <- td.traverse().nodes().asScala;
-          labels = node.getLabels().asScala mkString " ";
-          index = db.index().forNodes(NeighborhoodBasedSimilaritySearch.LABEL_INDEX_NAME, NeighborhoodBasedSimilaritySearch.FULL_TEXT.asJava)
-         ) {
-
-      // In case the node is indexed before, remove all occurrences of it so
-      // we don't get old or duplicated data
-      index.remove(node)
-
-      index.add(node, NeighborhoodBasedSimilaritySearch.LABEL_INDEX_NAME, labels)
-    }
+  def propagatedLabels(node: Node, propName: String, td: TraversalDescription, alpha: Double): Map[String, Double] = {
+    val label_weight_parts = for (
+      path <- td.traverse(node).iterator().asScala;
+      weight = pow(alpha, path.length());
+      label <- path.endNode().getProperty(propName).toString.split(" ")
+    ) yield (label, weight)
+    label_weight_parts.toList.groupBy(_._1).mapValues(x => x map (_._2) sum)
   }
 
 }
 
 object NeighborhoodBasedSimilaritySearch {
   val FULL_TEXT = Map(IndexManager.PROVIDER -> "lucene", "type" -> "fulltext")
-  val LABEL_INDEX_NAME = "labelstore"
+  def indexName(propName: String) = s"index-prop-$propName"
+  val LABEL_PROPERTY_NAME = "_labels"
 }
