85cc8683880dfb426cc0e354795930387b39d751
Wen Li <wen.li@ucl.ac.uk>
2016-11-20 23:02:48 +0000

    Move SearchHit out from the nested

--- neo4j-plugin/src/main/java/uk/ac/cdrc/mintsearch/neo4j/Neo4JProcedure.java
+++ neo4j-plugin/src/main/java/uk/ac/cdrc/mintsearch/neo4j/Neo4JProcedure.java
@@ -5,7 +5,6 @@ package uk.ac.cdrc.mintsearch.neo4j;
  */
 
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
 import org.neo4j.logging.Log;
 import org.neo4j.procedure.Context;
 
@@ -25,35 +24,5 @@ public abstract class Neo4JProcedure
     // standard log, normally found under `data/log/console.log`
     @Context
     public Log log;
-    /**
-     * This is the output record for our search procedure. All procedures
-     * that return results return them as a Stream of Records, where the
-     * records are defined like this one - customized to fit what the procedure
-     * is returning.
-     *
-     * These classes can only have public non-final fields, and the fields must
-     * be one of the following types:
-     *
-     * <ul>
-     *     <li>{@link String}</li>
-     *     <li>{@link Long} or {@code long}</li>
-     *     <li>{@link Double} or {@code double}</li>
-     *     <li>{@link Number}</li>
-     *     <li>{@link Boolean} or {@code boolean}</li>
-     *     <li>{@link org.neo4j.graphdb.Node}</li>
-     *     <li>{@link org.neo4j.graphdb.Relationship}</li>
-     *     <li>{@link org.neo4j.graphdb.Path}</li>
-     *     <li>{@link java.util.Map} with key {@link String} and value {@link Object}</li>
-     *     <li>{@link java.util.List} of elements of any valid field type, including {@link java.util.List}</li>
-     *     <li>{@link Object}, meaning any of the valid field types</li>
-     * </ul>
-     */
-    public class SearchHit{
-      public Long nodeId;
-      public SearchHit(Node node){
-        this.nodeId = node.getId();
-      }
-
-    }
 }
 
