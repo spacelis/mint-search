ecdfcdcfeff0e1ff084cca4a52ab5c6b8e73c80f
Wen Li <wen.li@ucl.ac.uk>
2016-11-21 02:26:51 +0000

    WIP: Query by a subgraph defined by neighbourhood

--- neo4j-plugin/src/main/scala/uk/ac/cdrc/mintsearch/ranking/GraphSearchQuery.scala
+++ neo4j-plugin/src/main/scala/uk/ac/cdrc/mintsearch/ranking/GraphSearchQuery.scala
@@ -2,8 +2,12 @@ package uk.ac.cdrc.mintsearch.ranking
 
 import java.io.{File, IOException}
 
+import org.neo4j.cypher.export.CypherResultSubGraph
+
+import collection.JavaConverters._
 import org.neo4j.graphdb.GraphDatabaseService
 import org.neo4j.graphdb.factory.GraphDatabaseFactory
+import org.neo4j.graphdb.traversal.TraversalDescription
 import uk.ac.cdrc.mintsearch.ranking.NeighbourBasedRanking.NodeId
 
 /**
@@ -17,14 +21,21 @@ case class GraphSearchQuery(db: GraphDatabaseService, dbStore: File) {
 }
 
 object GraphSearchQuery {
-  def fromCypher(query: String): GraphSearchQuery = {
+  def fromCypher(cypher: String): GraphSearchQuery = {
     val dbStore = mkTempDir()
     val db = new GraphDatabaseFactory().newEmbeddedDatabase(dbStore)
-    db.execute(query)
+    db.execute(cypher)
     new GraphSearchQuery(db, dbStore)
   }
 
-  def fromNodeNeighbourHood(nodeId: NodeId) = _
+  def fromNodeNeighbourHood(nodeId: NodeId)(implicit db: GraphDatabaseService, traversalDescription: TraversalDescription) = {
+    //FIXME We should reconsider how can we construct a subgraph for search
+    val subgraph = new CypherResultSubGraph()
+    for {
+      path <- traversalDescription.traverse(db.getNodeById(nodeId)).iterator().asScala
+      node = path.endNode()
+    } subgraph.add(node)
+  }
 
   val defaultTempDir = new File("/tmp")
   def mkTempDir(prefix: String = "mintsearch-", suffix: String = ".tmp.d", dir: File = defaultTempDir) = {
