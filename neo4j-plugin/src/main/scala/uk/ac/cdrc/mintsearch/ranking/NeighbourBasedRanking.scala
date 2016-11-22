509b3745befac40eb80f11835f88d0f0e131bab2
Wen Li <spacelis@gmail.com>
2016-11-22 15:05:11 +0000

    Add some doc strings

--- neo4j-plugin/src/main/scala/uk/ac/cdrc/mintsearch/ranking/NeighbourBasedRanking.scala
+++ neo4j-plugin/src/main/scala/uk/ac/cdrc/mintsearch/ranking/NeighbourBasedRanking.scala
@@ -1,16 +1,15 @@
 package uk.ac.cdrc.mintsearch.ranking
 
 import scala.math.max
-import org.neo4j.graphdb.traversal.{Evaluators, TraversalDescription, Uniqueness}
-import org.neo4j.graphdb.{Node, Path, RelationshipType}
+import org.neo4j.graphdb.traversal.{ Evaluators, TraversalDescription, Uniqueness }
+import org.neo4j.graphdb.{ Node, Path, RelationshipType }
 import uk.ac.cdrc.mintsearch.ranking.NeighbourBasedRanking._
 import org.neo4j.kernel.impl.traversal.MonoDirectionalTraversalDescription
 
 /**
-  * Created by ucfawli on 11/18/16.
-  */
-trait NeighbourBasedRanking extends Ranking{
-
+ * Created by ucfawli on 11/18/16.
+ */
+trait NeighbourBasedRanking extends Ranking {
 
   val traverDescription: TraversalDescription
 
@@ -22,7 +21,7 @@ trait NeighbourBasedRanking extends Ranking{
 
   }
 
-  def mkGraphDoc(nodeSet: Set[Node]) : GraphDoc = {
+  def mkGraphDoc(nodeSet: Set[Node]): GraphDoc = {
     implicit val nodeWrapper = NeighbourAwareNode.wrapNode(traverDescription)
     (for { n <- nodeSet } yield n.getId -> n.collectNeighbourLabels(propagate)).toMap
   }
@@ -35,47 +34,46 @@ object NeighbourBasedRanking {
   type NodeId = Long
 
   /**
-    * A mapping from label to a weight value
-    */
+   * A mapping from label to a weight value
+   */
   type WeightedLabelSet = Map[String, Double]
 
   /**
-    * A mapping from a node (by its ID) to a list of nodes (IDs)
-    */
+   * A mapping from a node (by its ID) to a list of nodes (IDs)
+   */
   type NodeMatching = Map[NodeId, Seq[NodeId]]
 
   /**
-    * A mapping from node to its weighted label set
-    */
+   * A mapping from node to its weighted label set
+   */
   type GraphDoc = Map[NodeId, WeightedLabelSet]
 
   /**
-    * A class for adding operators to Map[String, Double] aliased to WeightedLabelSet
-    * @param inner a value of type Map[String, Double]
-    */
+   * A class for adding operators to Map[String, Double] aliased to WeightedLabelSet
+   * @param inner a value of type Map[String, Double]
+   */
   class WeightedLabelSetWrapper(val inner: WeightedLabelSet) {
     def ---(other: WeightedLabelSetWrapper) = {
-      inner map { case (k, v) =>  (k, max(0.0, v - other.inner.getOrElse(k, 0.0)))}
+      inner map { case (k, v) => (k, max(0.0, v - other.inner.getOrElse(k, 0.0))) }
     }
 
   }
 
   /**
-    * Sum up a list of weight distributions
-    * @param xs a list of WeightedLabelSets
-    * @return a WeightedLabelSet in which the labels' weights are summed from xs
-    */
+   * Sum up a list of weight distributions
+   * @param xs a list of WeightedLabelSets
+   * @return a WeightedLabelSet in which the labels' weights are summed from xs
+   */
   def sum(xs: TraversableOnce[WeightedLabelSet]) =
     xs.flatMap(_.toSeq).toSeq.groupBy(_._1).mapValues(x => x.map(_._2).sum)
 
   /**
-    * Implicit wrapping a value of type WeightedLabelSet (Map[String, Double]) to provide additional operator on them
-    * @param wls a value of WeightedLabelSet
-    * @return a wrapped Map[String, Double]
-    */
+   * Implicit wrapping a value of type WeightedLabelSet (Map[String, Double]) to provide additional operator on them
+   * @param wls a value of WeightedLabelSet
+   * @return a wrapped Map[String, Double]
+   */
   implicit def asWightedLabelSetWrapper(wls: WeightedLabelSet): WeightedLabelSetWrapper =
     new WeightedLabelSetWrapper(wls)
-
 
   def neighbourhoodTraversalDescription(order: Int, relTypes: Seq[String]): TraversalDescription = {
     val td: TraversalDescription = new MonoDirectionalTraversalDescription()
