d630af9245e516285003e8d8f48c3eea085c5836
Wen Li <wen.li@ucl.ac.uk>
2016-11-30 01:05:39 +0000

    Fix some typos

--- neo4j-plugin/src/test/scala/uk/ac/cdrc/mintsearch/neo4j/TestFullTextIndexSpec.scala
+++ neo4j-plugin/src/test/scala/uk/ac/cdrc/mintsearch/neo4j/TestFullTextIndexSpec.scala
@@ -2,28 +2,29 @@ package uk.ac.cdrc.mintsearch.neo4j
 
 import org.scalatest._
 import org.neo4j.driver.v1._
-import org.neo4j.harness.TestServerBuilders
+import org.neo4j.harness.{ServerControls, TestServerBuilder, TestServerBuilders}
 
 /**
- * Created by ucfawli on 29-Nov-16.
+ * Testing the fulltext_index/search procedure
  */
+
 class TestFullTextIndexSpec extends FlatSpec {
 
   trait Neo4JFixture {
     private val _builder = TestServerBuilders.newInProcessBuilder()
-    def builder = _builder
-    lazy val neo4jServer = builder.newServer()
+    def builder: TestServerBuilder = _builder
+    lazy val neo4jServer: ServerControls = builder.newServer()
   }
 
   trait Neo4JFixtureFullTextSearchProcedure extends Neo4JFixture {
-    override val builder = super.builder.withProcedure(classOf[FullTextIndex])
+    override val builder: TestServerBuilder = super.builder.withProcedure(classOf[FullTextIndex])
   }
 
   "A test" should "run" in new Neo4JFixtureFullTextSearchProcedure {
     val driver: Driver = GraphDatabase.driver(neo4jServer.boltURI(), Config.build().withEncryptionLevel(Config.EncryptionLevel.NONE).toConfig)
     // Given I've started Neo4j with the FullTextIndex procedure class
     //       which my 'neo4j' rule above does.
-    val session = driver.session()
+    val session: Session = driver.session()
 
     // And given I have a node in the database
     val nodeId: Long = session.run("CREATE (p:User {name:'Brookreson'}) RETURN id(p)")
