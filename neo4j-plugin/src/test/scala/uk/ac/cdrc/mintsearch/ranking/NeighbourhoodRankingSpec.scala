899d93e64bd70af870e1f88eb53fbb38278383c4
Wen Li <wen.li@ucl.ac.uk>
2017-01-29 18:31:07 +0000

    Add a test for graph indexing and searching

--- neo4j-plugin/src/test/scala/uk/ac/cdrc/mintsearch/ranking/NeighbourhoodRankingSpec.scala
+++ neo4j-plugin/src/test/scala/uk/ac/cdrc/mintsearch/ranking/NeighbourhoodRankingSpec.scala
@@ -25,7 +25,7 @@ class NeighbourhoodRankingSpec extends fixture.WordSpec with Matchers {
 
       override val labelStorePropKey: String = s"__nagg_$radius"
       override val db: GraphDatabaseService = neo4jServer.graph()
-      override val indexName: String = "ness_index"
+      override val indexName: String = s"index-nagg-r$radius-p$propagationFactor"
     }
 
     val graphSearcher = new NeighbourhoodRanking
@@ -61,7 +61,7 @@ class NeighbourhoodRankingSpec extends fixture.WordSpec with Matchers {
   }
 
   "NeighbourhoodRanking" should {
-    "find a single sub graph" in { f =>
+    "find the only sub graph" in { f =>
       import f._
       WithResource(driver.session()) { session =>
         val res = session.run(
@@ -88,6 +88,120 @@ class NeighbourhoodRankingSpec extends fixture.WordSpec with Matchers {
             """.stripMargin)
           val top = graphSearcher.rankEmbeddings(q).toList.head
           top.nodeIds.toSet should be (Set(nodeA, nodeB, nodeC))
+          tx.success()
+        }
+      }
+    }
+    "find a single sub graph" in { f =>
+      import f._
+      WithResource(driver.session()) { session =>
+        val res = session.run(
+          """CREATE
+            | (a: Person {name:'Alice'}),
+            | (b: Person {name: 'Bob'}),
+            | (c: Person {name: 'Carl'}),
+            | (x: Person {name:'Xman'}),
+            | (y: Person {name: 'Yuri'}),
+            | (z: Person {name: 'Zebra'}),
+            | (a)-[:Friend]->(b)-[:Friend]->(c),
+            | (x)-[:Friend]->(y)-[:Friend]->(z)
+            | RETURN id(a), id(b), id(c)""".stripMargin
+        )
+          .single()
+        val Seq(nodeA, nodeB, nodeC) = for (i <- 0 to 2) yield res.get(i).asLong
+        WithResource(indexWriter.db.beginTx()) { tx =>
+          // query the neighbours
+          indexWriter.index(indexWriter.db.getNodeById(nodeA))
+          indexWriter.index(indexWriter.db.getNodeById(nodeB))
+          indexWriter.index(indexWriter.db.getNodeById(nodeC))
+          indexWriter.awaitForIndexReady()
+          val q = graphSearcher.fromCypherCreate(
+            """CREATE
+              | (a: Person {name: 'Alice'}),
+              | (b: Person {name: 'Bob'}),
+              | (a)-[:Friend]->(b)
+            """.stripMargin)
+          val res = graphSearcher.rankEmbeddings(q).toList
+          res should have length 1
+          res.head.nodeIds.toSet should be (Set(nodeA, nodeB, nodeC))
+          tx.success()
+        }
+      }
+    }
+    "find two sub graphs" in { f =>
+      import f._
+      WithResource(driver.session()) { session =>
+        val res = session.run(
+          """CREATE
+            | (a: Person {name:'Alice'}),
+            | (b: Person {name: 'Bob'}),
+            | (c: Person {name: 'Carl'}),
+            | (x: Person {name:'David'}),
+            | (y: Person {name: 'Alice'}),
+            | (z: Person {name: 'Bob'}),
+            | (a)-[:Friend]->(b)-[:Friend]->(c),
+            | (x)-[:Friend]->(y)-[:Friend]->(z)
+            | RETURN id(a), id(b), id(c), id(x), id(y), id(z)""".stripMargin
+        )
+          .single()
+        val Seq(nodeA, nodeB, nodeC, nodeX, nodeY, nodeZ) = for (i <- 0 until 6) yield res.get(i).asLong
+        WithResource(indexWriter.db.beginTx()) { tx =>
+          // query the neighbours
+          indexWriter.index(indexWriter.db.getNodeById(nodeA))
+          indexWriter.index(indexWriter.db.getNodeById(nodeB))
+          indexWriter.index(indexWriter.db.getNodeById(nodeC))
+          indexWriter.index(indexWriter.db.getNodeById(nodeX))
+          indexWriter.index(indexWriter.db.getNodeById(nodeY))
+          indexWriter.index(indexWriter.db.getNodeById(nodeZ))
+          indexWriter.awaitForIndexReady()
+          val q = graphSearcher.fromCypherCreate(
+            """CREATE
+              | (a: Person {name: 'Alice'}),
+              | (b: Person {name: 'Bob'}),
+              | (a)-[:Friend]->(b)
+            """.stripMargin)
+          val res = graphSearcher.rankEmbeddings(q).toList
+          res should have length 2
+          val resNodeSets = res.map(_.nodeIds.toSet)
+          resNodeSets should contain (Set(nodeA, nodeB, nodeC))
+          resNodeSets should contain (Set(nodeX, nodeY, nodeZ))
+          tx.success()
+        }
+      }
+    }
+
+    "find two sub graphs after batch indexing" in { f =>
+      import f._
+      WithResource(driver.session()) { session =>
+        val res = session.run(
+          """CREATE
+            | (a: Person {name:'Alice'}),
+            | (b: Person {name: 'Bob'}),
+            | (c: Person {name: 'Carl'}),
+            | (x: Person {name:'David'}),
+            | (y: Person {name: 'Alice'}),
+            | (z: Person {name: 'Bob'}),
+            | (a)-[:Friend]->(b)-[:Friend]->(c),
+            | (x)-[:Friend]->(y)-[:Friend]->(z)
+            | RETURN id(a), id(b), id(c), id(x), id(y), id(z)""".stripMargin
+        )
+          .single()
+        val Seq(nodeA, nodeB, nodeC, nodeX, nodeY, nodeZ) = for (i <- 0 until 6) yield res.get(i).asLong
+        WithResource(indexWriter.db.beginTx()) { tx =>
+          // query the neighbours
+          indexWriter.index()
+          indexWriter.awaitForIndexReady()
+          val q = graphSearcher.fromCypherCreate(
+            """CREATE
+              | (a: Person {name: 'Alice'}),
+              | (b: Person {name: 'Bob'}),
+              | (a)-[:Friend]->(b)
+            """.stripMargin)
+          val res = graphSearcher.rankEmbeddings(q).toList
+          res should have length 2
+          val resNodeSets = res.map(_.nodeIds.toSet)
+          resNodeSets should contain (Set(nodeA, nodeB, nodeC))
+          resNodeSets should contain (Set(nodeX, nodeY, nodeZ))
           tx.success()
         }
       }
