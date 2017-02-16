ae698d7ce7c4bb68f2de12e9e771d07b9bd0b017
Wen Li <spacelis@gmail.com>
2017-02-16 16:09:46 +0000

    Use static picklers for jsonfy

--- neo4j-plugin/src/main/scala/uk/ac/cdrc/mintsearch/index/LabelTypeContext.scala
+++ neo4j-plugin/src/main/scala/uk/ac/cdrc/mintsearch/index/LabelTypeContext.scala
@@ -1,9 +1,15 @@
 package uk.ac.cdrc.mintsearch.index
 
 import uk.ac.cdrc.mintsearch.WeightedLabelSet
+
 import scala.pickling._
-import Defaults._
 import json._
+import scala.pickling.Defaults.{arrayPickler, doubleArrayPickler, intPickler, pickleOps, refPickler, refUnpickler, stringPickler}
+import scala.pickling.static._
+
+// Do not use IntelliJ's Optimizing Imports. In case you did, recover from the following
+//import scala.pickling.Defaults.{arrayPickler, doubleArrayPickler, intPickler, pickleOps, refPickler, refUnpickler, stringPickler}
+//import scala.pickling.static._
 
 /**
   * Describe the label structure
@@ -24,8 +30,18 @@ trait KeyValueLabelType extends LabelTypeContext {
   override def labelEncode(label: L): String = s"${label._1}:${label._2}"
   override def labelEncodeQuery(label: L): String = s"${label._1}\\:${label._2}"
 
-  override def JSONfy(wls: WeightedLabelSet[(String, String)]): String =
-    wls.pickle.value
-  override def deJSONfy(json: String): WeightedLabelSet[(String, String)] =
-    JSONPickle(json).unpickle[WeightedLabelSet[(String, String)]]
+  case class KVLabelStore(ks: Array[String], vs: Array[String], ws: Array[Double])
+  implicit val KVLabelStorePickler: Pickler[KVLabelStore] = Pickler.generate[KVLabelStore]
+  implicit val KVLabelStoreUnpickler: Unpickler[KVLabelStore] = Unpickler.generate[KVLabelStore]
+
+  override def JSONfy(wls: WeightedLabelSet[(String, String)]): String = {
+    val (kvs, ws) = wls.toArray.unzip[(String, String), Double]
+    val (ks, vs) = kvs.unzip[String, String]
+    val toStore: KVLabelStore = KVLabelStore(ks, vs, ws)
+    toStore.pickle.value
+  }
+  override def deJSONfy(jsonObj: String): WeightedLabelSet[(String, String)] ={
+    val kvls = jsonObj.unpickle[KVLabelStore]
+    (kvls.ks zip kvls.vs zip kvls.ws).toMap
+  }
 }
---
+++ neo4j-plugin/src/test/scala/uk/ac/cdrc/mintsearch/index/KeyValueLabelTypeSpec.scala
@@ -0,0 +1,32 @@
+package uk.ac.cdrc.mintsearch.index
+
+import org.scalacheck.Arbitrary
+import org.scalacheck.Prop._
+import org.scalacheck.Arbitrary._
+import org.scalatest.{Matchers, WordSpec}
+import uk.ac.cdrc.mintsearch.WeightedLabelSet
+
+/**
+  * Test KeyValueLabelType
+  */
+class KeyValueLabelTypeSpec extends WordSpec with Matchers {
+
+//  implicit val arbEntry = for {
+//    a <- Arbitrary.arbString.arbitrary
+//    b <- Arbitrary.arbString.arbitrary
+//    c <- Arbitrary.arbDouble.arbitrary
+//  } yield ((a, b), c)
+//
+//  implicit val arbWLS = for {
+//    es <- Arbitrary.arbitrary[List[((String, String), Double)]]
+//  } yield es.toMap
+
+  "KeyValueLabelType" should {
+    "encode and decode WLS" in new KeyValueLabelType {
+      forAll {
+        (wls: Map[(String, String), Double]) =>
+          wls == deJSONfy(JSONfy(wls))
+      }
+    }
+  }
+}
